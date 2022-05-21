dba425fe7c5b4852fb1bfb010120b4f1f2ecdcd2
Ravi Panchal <panchalravi018@gmail.com>
2022-05-21 18:54:29 +0530

    dashboard with menu completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Game : Space Shooter
+#Author : Ravi Panchal
 
 import pygame
 from pygame.locals import *
@@ -63,6 +64,97 @@ help_sec = False
 dashboard = True
 chooseship_sec = False
 
+#author description
+def author():
+    font = pygame.font.SysFont('arial', 14, 2)
+    text = font.render('Developer : Ravi Panchal',True,WHITE)
+    textwidth = text.get_width()
+    textheight = text.get_height()
+    screen.blit(text,((width) - (textwidth + 20),(height - (textheight * 2))))
+
+#--------------dashboard------------------
+#this used to render image title and box
+#all operations are perform in loops event
+class Dashboard:
+    def __init__(self):
+        self.selectedlevel = 'LOW'
+        self.selectedship = 'assets/player/Ships/Small/1.png'
+        self.titlewidth = 160
+        self.titleheight = 30
+        self.titlex = width / 2 - self.titlewidth/2
+        self.titley = 10
+        self.surf = pygame.surface.Surface((self.titlewidth,self.titleheight))
+        self.sliderboxx = width - (width/2-75) + (width/2-100)
+        self.sliderboxy = (height/2) - ((height/2 + 30)/2)
+        self.slider = False
+        self.helpmsgx = 0
+        self.helpmsgy = 0
+        self.helpmsgscrolly = 0
+   
+        # return Planet
+    
+    def StartGame(self):
+        font = pygame.font.SysFont('arial',12,2)
+        title = font.render('LEVEL : '+str(self.selectedlevel),True,WHITE)
+        screen.blit(title, (width - title.get_width() - 5,10))
+
+        score = font.render('SCORE : '+str(1000),True,WHITE)
+        screen.blit(score,(width - title.get_width() - score.get_width() - 20 ,10))
+
+        ship = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(self.selectedship),(20,40)), 90)
+        screen.blit(ship, (width - title.get_width() - ship.get_width() - score.get_width() - 30 ,10))
+
+    def SettingSec(self):
+        self.surf.fill(DARKBLUE)
+        font = pygame.font.SysFont('arial',20,2)
+        title = font.render('Setting',True,WHITE)
+        #display background and title
+        screen.blit(setting_bg,(0,0))
+        screen.blit(self.surf,(self.titlex,self.titley))
+        screen.blit(title,((self.titlex + (self.titlewidth/2) - (title.get_width()/2)),(self.titley + (self.titleheight / 2) - (title.get_height() / 2))))
+        
+        
+    def ScoreSec(self):
+        self.surf.fill(DARKBLUE)
+        font = pygame.font.SysFont('arial',20,2)
+        title = font.render('Score Board',True,WHITE)
+        #display background and title
+        screen.blit(score_bg,(0,0))
+        screen.blit(self.surf,(self.titlex,self.titley))
+        screen.blit(title,((self.titlex + (self.titlewidth/2) - (title.get_width()/2)),(self.titley + (self.titleheight / 2) - (title.get_height() / 2))))
+    
+    def AboutSec(self):
+        self.surf.fill(DARKBLUE)
+        font = pygame.font.SysFont('arial',20,2)
+        title = font.render('About Us',True,WHITE)
+        #display background and title
+        screen.blit(about_bg,(0,0))
+        screen.blit(self.surf,(self.titlex,self.titley))
+        screen.blit(title,((self.titlex + (self.titlewidth/2) - (title.get_width()/2)),(self.titley + (self.titleheight / 2) - (title.get_height() / 2))))
+
+    def HelpSec(self):
+        self.surf.fill(DARKBLUE)
+        font = pygame.font.SysFont('arial',20,2)
+        title = font.render('Help !',True,WHITE)
+        #display background and title
+        screen.blit(help_bg,(0,0))
+        screen.blit(self.surf,(self.titlex,self.titley))
+        screen.blit(title,((self.titlex + (self.titlewidth/2) - (title.get_width()/2)),(self.titley + (self.titleheight / 2) - (title.get_height() / 2))))
+
+    def ChooseShipSec(self):
+        self.surf.fill(DARKBLUE)
+        font = pygame.font.SysFont('arial',20,2)
+        title = font.render('Spaceship',True,WHITE)
+        #display background and title
+        screen.blit(about_bg,(0,0))
+        screen.blit(self.surf,(self.titlex,self.titley))
+        screen.blit(title,((self.titlex + (self.titlewidth/2) - (title.get_width()/2)),(self.titley + (self.titleheight / 2) - (title.get_height() / 2))))
+
+        
+
+
+#create dashboard object
+db = Dashboard()
 
 #init.. True for loop
 run = True
@@ -70,9 +162,170 @@ run = True
 while run:
     clock.tick(FPS)
 
-    screen.fill(DARKBLUE)
-    screen.blit(dashboard_bg,(0,0))
+    #if game is not start then we are on dashboard
+    if startgame == False and dashboard == True:
+        screen.fill(DARKBLUE)
+        screen.blit(dashboard_bg,(0,0))
+        author()
+        #show title and selectd ship for player ===> Player Ship
+        db.PlayerShipAndLevel()
         
+        #score slider display
+        db.ScoreSlider()
+        #setup menu
+        #start button
+        start.msg(screen,'To Start Game Click Here',12)
+
+        if start.draw(screen):
+            startgame = True
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = False 
+            dashboard = False
+            chooseship_sec = False
+
+        #setting button
+        setting.msg(screen,'Choose a best settings',12)
+        if setting.draw(screen):
+            startgame = False
+            setting_sec = True
+            score_sec = False
+            help_sec = False
+            about_sec = False 
+            dashboard = False
+            chooseship_sec = False
+
+        #score button
+        score.msg(screen,'Show Score',12)
+        if score.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = True
+            help_sec = False
+            about_sec = False 
+            dashboard = False
+            chooseship_sec = False
+
+        #about button
+        about.msg(screen,'Know more',12)
+        if about.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = True 
+            dashboard = False
+            chooseship_sec = False
+
+        #help button
+        Help.msg(screen,'Click to Get Tips',12)
+        if Help.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = True
+            about_sec = False 
+            dashboard = False
+            chooseship_sec = False
+        
+        #chooseship button
+        chooseship.msg(screen,'select your space-ship',12)
+        if chooseship.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = False 
+            dashboard = False
+            chooseship_sec = True
+
+        #exit button
+        if Exit.draw(screen):
+            run = False
+    #open setting section
+    elif setting_sec:
+        screen.fill(LIGHTGREEN)
+        db.SettingSec()
+        if back.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = False
+            dashboard = True
+            chooseship_sec = False
+
+    #open score section
+    elif score_sec:
+        screen.fill(LIGHTGREEN)
+        db.ScoreSec()
+        if back.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = False
+            dashboard = True
+            chooseship_sec = False
+
+    #open about section
+    elif about_sec:
+        screen.fill(RED)
+        db.AboutSec()
+        if back.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = False
+            dashboard = True
+            chooseship_sec = False
+        
+    #open help section
+    elif help_sec:
+        screen.fill(BLACK)
+        db.HelpSec()
+        # if btup.draw(screen):
+        #     db.helpmsgy += 10
+        # if btdown.draw(screen):
+        #     db.helpmsgy -= 10
+        if back.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = False
+            dashboard = True
+            chooseship_sec = False
+    
+    #open chooseship section
+    elif chooseship_sec:
+        screen.fill(BLACK)
+        db.ChooseShipSec()
+        if back.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = False
+            dashboard = True
+            chooseship_sec = False
+        
+    #it means game is started
+    else:
+        screen.fill(WHITE)
+        
+        if back.draw(screen):
+            startgame = False
+            setting_sec = False
+            score_sec = False
+            help_sec = False
+            about_sec = False
+            dashboard = True
+            chooseship_sec = False
+            
+
     #keyboard event loop start here
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
